cc5c139fdf50932cf657cc22e68ced126711c833
Logan Tegman <ltegman@gmail.com>
2016-01-08 12:58:02 -0800

    Add support for paletted bitmaps

--- index.js
+++ index.js
@@ -16,18 +16,29 @@ fs.readFile(__dirname + '/test/' + options.file, (err, data) => {
   if (!options) return console.log('Invalid arguments');
 
   console.log(headers);
-  const transformed = bruteTransform(data, headers, options.transform);
+
+  // palette
+  let transformed;
+  if (headers.pixelOffset - headers.endOfHeaders > 12) {
+    transformed = paletteTransform(data, headers, options.transform);
+  }
+  // non-palette
+  if (headers.pixelOffset - headers.endOfHeaders <= 12) {
+    transformed = bruteTransform(data, headers, options.transform);
+  }
+
   fs.writeFile('output.bmp', transformed, () => {});
 });
 
-function bruteTransform(image, headers, transform) {
+function paletteTransform(image, headers, transform) {
   // avoid mutating passed in buffer
   const newImage = image.slice(0);
 
-  // transform pixels
-  for (let i = headers.pixelOffset; i < newImage.length; i += 3) {
+  // transform palette
+  for (let i = headers.endOfHeaders; i < headers.pixelOffset; i += 3) {
     // keep transforms out of function calls to avoid perfromance hit
 
+    // rgb transform
     if (typeof transform === 'object') {
       let newColors = {
         r: transform.r + newImage[i],
@@ -47,6 +58,52 @@ function bruteTransform(image, headers, transform) {
       newImage[i + 2] = newColors.b;
     }
 
+    // color inversion transform
+    if (transform === 'invert') {
+      newImage[i] = 255 - newImage[i];
+      newImage[i + 1] = 255 - newImage[i + 1];
+      newImage[i + 2] = 255 - newImage[i + 2];
+    }
+
+    for (let i2 = i; i2 < i + 3; i2++) {
+      if (newImage[i2] > 255) newImage[i2] = 255;
+      if (newImage[i2] < 0) newImage[i2] = 0;
+    }
+  }
+
+  return newImage;
+
+}
+
+function bruteTransform(image, headers, transform) {
+  // avoid mutating passed in buffer
+  const newImage = image.slice(0);
+
+  // transform pixels
+  for (let i = headers.pixelOffset; i < newImage.length; i += 3) {
+    // keep transforms out of function calls to avoid perfromance hit
+
+    // rgb transform
+    if (typeof transform === 'object') {
+      let newColors = {
+        r: transform.r + newImage[i],
+        g: transform.g + newImage[i + 1],
+        b: transform.b + newImage[i + 2]
+      };
+
+      for (var color in newColors) {
+        if (newColors.hasOwnProperty(color)) {
+          if (newColors[color] > 255) newColors[color] = 255;
+          if (newColors[color] < 0) newColors[color] = 0;
+        }
+      }
+
+      newImage[i] = newColors.r;
+      newImage[i + 1] = newColors.g;
+      newImage[i + 2] = newColors.b;
+    }
+
+    // color inversion transform
     if (transform === 'invert') {
       newImage[i] = 255 - newImage[i];
       newImage[i + 1] = 255 - newImage[i + 1];
