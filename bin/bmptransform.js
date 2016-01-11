4eef815a22eb0a1f923fe6d86364a147e3136d86
Logan Tegman <ltegman@gmail.com>
2016-01-11 12:57:39 -0800

    Consolidate options error checking

--- bin/bmptransform.js
+++ bin/bmptransform.js
@@ -12,22 +12,18 @@ const bitmapTransformer = require(path.join(__dirname,
 const options = getArgs(process.argv);
 const output = options.output || 'transformed.bmp';
 
-if (!options.file || !options.transform) {
+if (!options || !options.file || !options.transform) {
   return console.log('Must pass a valid .bmp file and transform');
 }
 
 fs.readFile(path.resolve(process.cwd(), options.file), (err, data) => {
   if (err) return console.log(err);
 
-  if (!options) {
-    return console.log('Invalid arguments: Please specify a file' +
-      ' location and transformation');
-  }
-
   const transformed = bitmapTransformer(data, options.transform);
   const headers = getHeaders(data);
 
   console.log(headers);
 
-  fs.writeFile(path.resolve(process.cwd(), output), transformed, () => {});
+  fs.writeFile(path.resolve(process.cwd(), output), transformed,
+    (err) => console.log(err));
 });
